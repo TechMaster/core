39f1587037ed44dd7e4e2106b642a297dbb1ee03
techmaster <cuong@techmaster.vn>
2021-08-25 07:51:28 +0700

    v0.1.1

--- resto/resto.go
+++ resto/resto.go
@@ -9,30 +9,40 @@ import (
 	"github.com/hashicorp/go-retryablehttp"
 )
 
-func Post(url string, data interface{}) (*http.Response, error) {
+type Resto struct {
+	retry *retryablehttp.Client
+}
+
+/*
+Tạo Retry HTTP Client với tham số
+tryTimes: số lần cố gắng thử lại
+waitMax: thời gian đợi tối đa sau mỗi lần tính bằng millisecond
+*/
+func Retry(tryTimes int, waitMax time.Duration) Resto {
+	retryHTTP := retryablehttp.NewClient()
+	retryHTTP.RetryMax = tryTimes
+	retryHTTP.RetryWaitMax = waitMax * time.Millisecond
+	retryHTTP.Logger = nil
+	return Resto{
+		retry: retryHTTP,
+	}
+}
+
+func (r Resto) Post(url string, data interface{}) (*http.Response, error) {
 	jsonstr, err := json.Marshal(data)
 	if err != nil {
 		return nil, err
 	}
-	restclient := retryablehttp.NewClient()
-	restclient.RetryMax = 4
-	restclient.RetryWaitMax = 100 * time.Millisecond
-	restclient.Logger = nil
 
-	response, err := restclient.Post(url, "application/json", bytes.NewBuffer(jsonstr))
+	response, err := r.retry.Post(url, "application/json", bytes.NewBuffer(jsonstr))
 	if err != nil {
 		return nil, err
 	}
 	return response, nil
 }
 
-func Get(url string) (*http.Response, error) {
-	restclient := retryablehttp.NewClient()
-	restclient.RetryMax = 4
-	restclient.RetryWaitMax = 100 * time.Millisecond
-	restclient.Logger = nil
-
-	response, err := restclient.Get(url)
+func (r Resto) Get(url string) (*http.Response, error) {
+	response, err := r.retry.Get(url)
 	if err != nil {
 		return nil, err
 	}
