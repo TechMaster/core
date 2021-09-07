caf5027aa43b7912737875fcfa9b8cf3cd70e6a9
japangermany1998 <nhatduc@techmaster.vn>
2021-09-07 21:17:27 +0700

    v0.1.32

--- session/session.go
+++ session/session.go
@@ -19,7 +19,7 @@ const (
 var Sess *sessions.Sessions         //Cấu hình Session Manager
 var redisDB *redis_session.Database //Đây là một wrapper nối xuống Redis của Iris
 var redisClient *redis.Client       //Đây là redis client trực tiếp nối xuống Redis db không qua Iris
-var expires = time.Hour * 8760         //Thời gian mà 1 session sẽ hết hạn và bị xoá khỏi Redis
+var expires = time.Hour * 720         //Thời gian mà 1 session sẽ hết hạn và bị xoá khỏi Redis
 
 /* Khởi tạo In Memory Session, không kết nối vào Redis hay bất kỳ CSDL nào
 Dùng trong ứng dụng đơn lẻ
