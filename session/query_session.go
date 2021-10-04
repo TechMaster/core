cfc9936271cf5ef395d86ed31cccda83532a425f
japangermany1998 <nhatduc@techmaster.vn>
2021-10-04 18:01:18 +0700

    v0.1.41

--- session/query_session.go
+++ session/query_session.go
@@ -12,11 +12,7 @@ import (
 Lấy thông tin đăng nhập của người dùng từ Redis Session
 */
 func GetAuthInfoSession(ctx iris.Context) (authinfo *pmodel.AuthenInfo) {
-	sessid := ctx.GetHeader("token")
-	if sessid == "" {
-		return nil
-	}
-	data := sessions.Get(ctx).GetByToken(SESS_USER, sessid)
+	data := sessions.Get(ctx).Get(SESS_USER)
 	if data == nil {
 		return nil
 	}
